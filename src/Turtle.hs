17c1ce408af5af531fea07250720b3de108c9ed8
u6826541 <u6826541@anu.edu.au>
2019-04-30 00:13:54 +1000

    turtle picture

--- src/Turtle.hs
+++ src/Turtle.hs
@@ -1,3 +1,13 @@
+{-|
+Module      : <Turtle>
+Description : <Turtle draws picture according to commands>
+Maintainer  : <u6826541@anu.edu.an>
+Name        : <Yixi Rao>
+Assignment  : <2>
+
+<Turtle draws picture according to commands.>
+
+-}
 module Turtle where
 
 import CodeWorld hiding (polygon)
@@ -25,12 +35,11 @@ triangle l =[Turn ((-2/3)*pi), Forward l ,Turn ((-2/3)*pi) ,Forward l ,Turn ((-2
 polygon :: Int -> Double -> [TurtleCommand]
 polygon n len = helpPolygon n n len
 
--- | is a function , which will help to build a polygon with an another n sides input as to help to define the angle
+-- | A helper function,which will help to build a polygon with an another n sides input as to help to define the angle
 helpPolygon:: Int -> Int -> Double -> [TurtleCommand]
 helpPolygon d m s
     |m == 3 = [PenDown,Turn angle,Forward s,Turn angle,Forward s,Turn angle,Forward s,PenUp]
     |otherwise = [PenDown,Turn angle,Forward s,PenUp]++(helpPolygon d (m-1) s)
-
     where angle= (2*pi)/(fromIntegral d)
 
 -- Task 2: Interpreting Turtle Commands
@@ -55,33 +64,31 @@ initialState = Turtle (0,0) (pi/2) PenDown MyPenDown
 runTurtle :: [TurtleCommand] -> Picture
 runTurtle command= stateToPic (comToState command initialState)
 
-         -- TODO
 
 -- | Transform the commands to the list of TurtleStates
 comToState :: [TurtleCommand]-> TurtleState-> [TurtleState]
 comToState l (Turtle (a,b) ir tc ud) = case l of
-            x:xs ->case x of
-                PenDown -> [Turtle (a,b) ir PenDown MyPenDown]++(comToState xs (Turtle (a,b) ir tc MyPenDown))
-                PenUp -> [Turtle (a,b) ir PenUp MyPenUp] ++ (comToState xs (Turtle (a,b) ir tc MyPenUp))
-                Forward d ->[Turtle (a,b) ir (Forward d) ud]++ (comToState xs (Turtle (newX,newY) ir (Forward d) ud))
-                    where newX = (a+d*(cos ir))
-                          newY = (b+d*(sin ir))
-                Turn r -> [Turtle (a,b) (r+ir) (Turn (r+ir)) ud] ++ (comToState xs (Turtle (a,b) (r+ir) (Turn (r+ir)) ud))
-            _-> []
+    [] -> []
+    PenDown : xs -> [Turtle (a,b) ir PenDown MyPenDown]++(comToState xs (Turtle (a,b) ir tc MyPenDown))
+    PenUp : xs -> [Turtle (a,b) ir PenUp MyPenUp] ++ (comToState xs (Turtle (a,b) ir tc MyPenUp))
+    Forward d : xs->[Turtle (a,b) ir (Forward d) ud]++ (comToState xs (Turtle (newX,newY) ir (Forward d) ud))
+            where newX = (a+d*(cos ir))
+                  newY = (b+d*(sin ir))
+    Turn r : xs -> [Turtle (a,b) (nr) (Turn (nr)) ud] ++ (comToState xs (Turtle (a,b) (nr) (Turn (nr)) ud))
+            where nr = r+ir
+
 
 -- | This function turns all the states to picture according to the pen's situation
 stateToPic :: [TurtleState] -> Picture
 stateToPic list = case list of
-            [] -> coordinatePlane
-            x:xs -> case x of
-                Turtle (a,b) r (Forward d) ud->case ud of
-                    MyPenDown -> ( polyline [(a,b),(newX,newY)])& (stateToPic xs)
-                    MyPenUp -> ( polyline [(newX,newY),(newX,newY)])& (stateToPic xs)
-                    where newX = (a+d*(cos r))
-                          newY = (b+d*(sin r))
-                _ -> stateToPic xs
-
-
+    [] -> coordinatePlane
+    Turtle (a,b) r (Forward d) MyPenDown : xs -> ( polyline [(a,b),(newX,newY)])& (stateToPic xs)
+            where newX = (a+d*(cos r))
+                  newY = (b+d*(sin r))
+    Turtle (a,b) r (Forward d) MyPenUp : xs -> ( polyline [(newX,newY),(newX,newY)])& (stateToPic xs)
+            where newX = (a+d*(cos r))
+                  newY = (b+d*(sin r))
+    _ :xs-> stateToPic xs
 
 
 
@@ -89,20 +96,20 @@ stateToPic list = case list of
 --   COMP1100: Implement this directly (Task 3A)
 --   COMP1130: Implement this using an L-System (Task 3B)
 
--- |
+-- | Generate a list of commands to draw a sierpinski triangle
 sierpinski :: Int -> Double -> [TurtleCommand]
 sierpinski n l = draw n n l
 
-
+-- | This helper function will generate a list, which will draw the sierpinski triangle
 draw:: Int ->Int-> Double -> [TurtleCommand]
 draw n k l
-   | n == 1 = [Forward len,Turn (pi*2/3),Forward len,Turn (pi*2/3),Forward len,Turn (pi*2/3)]
-   | otherwise = draw (n-1) k l ++ [f] ++ draw (n-1) k l ++ [tp ,f,tn] ++ draw (n-1) k l ++ [tn,f ,tp]
-    where len = l / (2 ** (fromIntegral (k-1)))
-          move = l / (2 ** (fromIntegral (k-(n-1))))
-          tp = Turn (pi*2/3)
-          tn = Turn (-pi*2/3)
-          f = Forward move
+    | n == 1 = [Forward len,Turn (pi*2/3),Forward len,Turn (pi*2/3),Forward len,Turn (pi*2/3)]
+    | otherwise = draw (n-1) k l ++ [f] ++ draw (n-1) k l ++ [tp ,f,tn] ++ draw (n-1) k l ++ [tn ,f ,tp]
+            where len = l / (2 ** (fromIntegral (k-1)))
+                  move = l / (2 ** (fromIntegral (k-(n-1))))
+                  tp = Turn (pi*2/3)
+                  tn = Turn (-pi*2/3)
+                  f = Forward move
 -- Task 3B: L-Systems (COMP1130 Only)
 
 lSystemCommands :: [TurtleCommand]
@@ -189,3 +196,4 @@ comp1100 = concat [start, c, o, m, p, one, one, o, o]
 
     -- Diagonal length of a right-angle triangle with both sides 0.5
     d = sqrt (2 * 0.5 * 0.5)
+
--- test/TurtleTest.hs
+++ test/TurtleTest.hs
@@ -12,6 +12,7 @@ tests =
   , exampleTestThree
   , exampleTestFour
   , exampleFailure
+  , exampleTestFive
   ]
 
 -- | Example test case. The String argument to 'Test' is a label that
@@ -21,14 +22,17 @@ tests =
 -- space to say things like "driving out and back leaves you in the
 -- same position".
 exampleTestOne :: Test
-exampleTestOne = Test "2 + 2 == 4" (assertEqual (2 + 2) (4 :: Int))
+exampleTestOne = Test "decago will have 10 sides" (assertEqual (totalSide (polygon 10 9)) (10 ))
+
 
 -- | Because we are storing the turtle position in a pair of
 -- 'Double's, addition behaves in ways you might not expect. The
 -- 'assertApproxEqual' function tests that two 'Double's are close
 -- enough to each other.
 exampleTestTwo :: Test
-exampleTestTwo = Test "0.1 + 0.2 =~ 0.3" (assertApproxEqual (0.1 + 0.2) 0.3)
+exampleTestTwo = Test label (assertApproxEqual (turtleDistance (triangle 5)) 15.0)
+        where label = "The perimeter of triangle with length 5 is 15"
+
 
 -- | 0.1 + 0.2 is NOT 0.3 when computed using 'Double's. This is why
 -- we have provided you with the 'assertApproxEqual' function.
@@ -37,14 +41,43 @@ exampleTestThree = Test "0.1 + 0.2 /= 0.3" (assertNotEqual (0.1 + 0.2) (0.3 :: D
 
 -- | You might find it easier to write tests against entire 'Point's.
 exampleTestFour :: Test
-exampleTestFour = Test "(0.0, 0.1 + 0.2) =~ (0.0, 0.3)"
-  (assertPointApproxEqual (0.0, 0.1 + 0.2) (0.0, 0.3))
+exampleTestFour = Test "decago driving out and back leaves you in the same position"
+  (assertPointApproxEqual ( lastPoint (comToState (polygon 10 9) initialState)) (0.0, 0.0))
+
+exampleTestFive :: Test
+exampleTestFive = Test label (assertApproxEqual (turtleDistance (polygon 10 5)) 50.0)
+        where label = "The perimeter of decago with length 5 is 50"
 
 -- | This test will fail, so you can see what a failing test looks
 -- like.
 exampleFailure :: Test
-exampleFailure = Test "0.1 + 0.2 == 0.3" (assertEqual (0.1 + 0.2) (0.3 :: Double))
+exampleFailure = Test str ((assertEqual (totalTriangle (sierpinski 10 10))) (19683))
+        where str = "The total amount of smallest equilateral triangle in Sierpinski's Triangle with depth 10"
 
+
+lastPoint :: [TurtleState] -> (Double,Double)
+lastPoint s = case s of
+    []-> (0,0)
+    [Turtle p _ _ _] -> p
+    _:xs -> lastPoint xs
+
+turtleDistance :: [TurtleCommand] -> Double
+turtleDistance tc = case tc of
+    [] -> 0
+    (Forward l):xs -> l+(turtleDistance xs)
+    _:xs -> turtleDistance xs
+
+totalSide :: [TurtleCommand] -> Int
+totalSide tc = case tc of
+    [] -> 0
+    Turn _ : Forward _ : xs -> 1 + totalSide xs
+    _ : xs-> totalSide xs
+
+totalTriangle :: [TurtleCommand] -> Int
+totalTriangle tc = case tc of
+    [] -> 0
+    Forward _: Turn _:Forward _: Turn _:Forward _: Turn _ :xs -> 1 + totalTriangle xs
+    _:xs -> totalTriangle xs
 -- | A haskell program starts by running the computation defined by
 -- 'main'. We run the list of tests that we defined above.
 main :: IO ()
